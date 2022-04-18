cb6f766a2f2593680748c0b8b180ae080b7849c6
heck <ramazan280699@gmail.com>
2022-04-18 23:34:13 +0300

    refactor: удалить ненужные классы

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmDoesNotExistsException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exceptions;
-
-public class FilmDoesNotExistsException extends RuntimeException{
-    public FilmDoesNotExistsException(String message) {
-        super(message);
-    }
-}
